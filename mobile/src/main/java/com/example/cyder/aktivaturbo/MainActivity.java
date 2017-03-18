516f2497453719662c6d5ef1eed0858cf566def3
kousuke nezu <kousuke.n2132@gmail.com>
2017-03-18 22:54:39 +0900

    [add] android側の通信する機構の作成
    ・PCCommunicateの作成

--- mobile/src/main/java/com/example/cyder/aktivaturbo/MainActivity.java
+++ mobile/src/main/java/com/example/cyder/aktivaturbo/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.cyder.aktivaturbo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     MobileCommunicate mobileCommunicate;
+
+    private final PCCommunicate mMessageListener = new PCCommunicate(8888) {
+        @Override
+        protected void onReceivedData(final String data) {
+            Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 //        通信用クラスのインスタンス化
         mobileCommunicate = new MobileCommunicate();
         mobileCommunicate.connect(this);
+
+        mMessageListener.start();
     }
 
     @Override
--- pc/src/com/example/cyder/aktivaturbopc/AktivaTurboPC.java
+++ pc/src/com/example/cyder/aktivaturbopc/AktivaTurboPC.java
@@ -1,10 +1,25 @@
 package com.example.cyder.aktivaturbopc;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+/**
+ * PCâ†â†’ã‚¹ãƒãƒ›ã®é€šä¿¡ã‚’è¡Œã†ã‚¢ãƒ—ãƒªã§ã™
+ * @author kousuke nezu
+ *
+ */
 public class AktivaTurboPC {
 
-	public static void main(String[] args) {
-		// TODO ©“®¶¬‚³‚ê‚½ƒƒ\ƒbƒhEƒXƒ^ƒu
-
+	public static void main(String[] args) throws IOException {
+		try (final Socket socket = new Socket("localhost", 9999)) {
+			final OutputStream outputStream = socket.getOutputStream();
+			final BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(outputStream));
+			writer.append("Hello!");
+			writer.flush();
+		}
 	}
-
 }
