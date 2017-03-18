488bad959edebe482f89032fce80ca4f5750e10e
kousuke nezu <kousuke.n2132@gmail.com>
2017-03-18 20:24:03 +0900

    [add] PC側アプリのファイル作成

--- mobile/src/main/java/com/example/cyder/aktivaturbo/PCCommunicate.java
+++ mobile/src/main/java/com/example/cyder/aktivaturbo/PCCommunicate.java
@@ -1,8 +1,10 @@
 package com.example.cyder.aktivaturbo;
 
 /**
+ * pcと通信をするクラスです
  * Created by kousuke nezu on 2017/03/18.
  */
 
 public class PCCommunicate {
+
 }
---
+++ pc/src/com/example/cyder/aktivaturbopc/AktivaTurboPC.java
@@ -0,0 +1,10 @@
+package com.example.cyder.aktivaturbopc;
+
+public class AktivaTurboPC {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
